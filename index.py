0c0935e85aea11fc78dc501f4dbdb91ffa6b6b67
Clément Gace <clementgace@outlook.com>
2026-05-22 12:06:10 +0200

    <feat> Produire html dans py

--- index.py
+++ index.py
@@ -8,6 +8,16 @@
 
 
 # Produire un fichier HTML
-
-
-
+with open("index.html", mode= "w", encoding="utf-8", newline="") as web:
+    web.write('<!DOCTYPE HTML>')
+    web.write('<HTML lang="FR">')
+    web.write('    <head>')
+    web.write('        <meta charset="utf-8">')
+    web.write('        <title> Requête citations </title>')
+    web.write('         <link href="style.css" rel="stylesheet" >')
+    web.write('          <h1>Citations<h1>')
+    web.write('     </head>')
+    web.write('     <body>')
+    web.write('         ')
+    web.write('    </body>')
+    web.write('</HTML>')
