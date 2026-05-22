c7fe39f54ac5188746f1f0354ff9da571b14fa6e
Clément Gace <clementgace@outlook.com>
2026-05-22 15:50:25 +0200

    <feat>ajout contenu des citations dans html

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import requests
 import html
 
 # Requeter API
+i= 0
 citations = []
 
 for i in range(5):
@@ -9,23 +10,29 @@ for i in range(5):
     data = response.json()
     citations.append((data['content'],data['author']))
 
-print(citations)
+# print(citations)
 
 # Traduire API (trier les informations que l'on souhaite garder)
 
 
 
 # Produire un fichier HTML
+# print (citations)
 with open("index.html", mode= "w", encoding="utf-8", newline="") as web:
-    web.write('<!DOCTYPE HTML>')
-    web.write('<HTML lang="FR">')
-    web.write('    <head>')
-    web.write('        <meta charset="utf-8">')
-    web.write('        <title> Requête citations </title>')
-    web.write('         <link href="style.css" rel="stylesheet" >')
-    web.write('          <h1>Citations<h1>')
-    web.write('     </head>')
-    web.write('     <body>')
-    web.write('         ')
-    web.write('    </body>')
-    web.write('</HTML>')
+    web.write('<!DOCTYPE HTML>\n')
+    web.write('<HTML lang="FR">\n')
+    web.write('    <head>\n')
+    web.write('        <meta charset="utf-8">\n')
+    web.write('        <title> Requête citations </title>\n')
+    web.write('         <link href="style.css" rel="stylesheet" >\n')
+    web.write('            <h1> Citations </h1>')
+    web.write('     </head>\n')
+    web.write('     <body>\n')
+    for i in range(5) :
+         
+         web.write(f'<div><p>{citations[i][0]} </p>')
+         web.write(f'<p>{citations[i][1]} </p></div>')
+    web.write('    </body>\n')
+    web.write('</HTML>\n')
+    
+    
